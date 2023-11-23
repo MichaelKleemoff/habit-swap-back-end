9f74a80091342122a263264ff189ae0f5920403a
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-22 21:25:16 -0500

    Add 'good_habits_bad_habits' table to 'db/schema.sql'. Update 'db/seed.sql'.

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS habit_swap_dev;
 CREATE DATABASE habit_swap_dev;
-\ c habit_swap_dev;
+\c habit_swap_dev;
 CREATE TABLE bad_habits (
 	id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL,
@@ -25,6 +25,13 @@ CREATE TABLE good_habits (
 	CHECK (
 		rating >= 0
 		AND rating <= 10
-	),
-	bad_habit_id INTEGER REFERENCES bad_habits (id) ON DELETE CASCADE
+	)
+);
+-- We want to relate and keep track of all the good habits to any bad habits
+-- 1. Reference PK of bad_habits
+-- 2. Reference PK of good_habits
+CREATE TABLE good_habits_bad_habits (
+	id SERIAL PRIMARY KEY,
+	bad_habit_id INTEGER REFERENCES bad_habits (id) ON DELETE CASCADE,
+	good_habit_id INTEGER REFERENCES good_habits (id) ON DELETE CASCADE
 );
---
+++ db/seed.sql
@@ -0,0 +1,69 @@
+\c habit_swap_dev;
+INSERT INTO bad_habits (
+		name,
+		img_url,
+		category,
+		description,
+		is_my_habit,
+		rating
+	)
+VALUES (
+		'smoking',
+		'image.jpg',
+		'health',
+		'Smoking causes cancer',
+		'true',
+		10
+	),
+	(
+		'drinking',
+		'image.jpg',
+		'health',
+		'Smoking causes cancer',
+		'true',
+		10
+	),
+	(
+		'overeating',
+		'image.jpg',
+		'health',
+		'Smoking causes cancer',
+		'true',
+		10
+	);
+INSERT INTO good_habits (
+		name,
+		img_url,
+		category,
+		description,
+		is_my_habit,
+		rating
+	)
+VALUES (
+		'nicorette gum',
+		'image.jpg',
+		'health',
+		'Nicorette gum helps to deal with nicotine cravings',
+		'true',
+		10
+	),
+	(
+		'meditation',
+		'image.jpg',
+		'health',
+		'Nicorette gum helps to deal with nicotine cravings',
+		'true',
+		10
+	),
+	(
+		'exercise',
+		'image.jpg',
+		'health',
+		'Nicorette gum helps to deal with nicotine cravings',
+		'true',
+		10
+	);
+INSERT INTO good_habits_bad_habits (bad_habit_id, good_habit_id)
+VALUES ('1', '1'),
+	('1', '2'),
+	('1', '3');
